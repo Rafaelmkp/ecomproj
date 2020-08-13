597cb401e22c4d6a509417f5ad3d00efda988997
Rafaelmkp <rafaelmobarack@gmail.com>
2020-08-12 22:23:19 -0300

    CRUD categories

--- db_files/db_ecommerce.sql
+++ db_files/db_ecommerce.sql
@@ -423,3 +423,30 @@ WHERE
 	DATE_ADD(upr.dtregister, INTERVAL 1 HOUR) >= NOW();
 
 SELECT * FROM tb_userspasswordsrecoveries;
+
+DELIMITER $$
+CREATE PROCEDURE `sp_categories_save` (
+pidcategory INT,
+pdescategory VARCHAR(64)
+)
+BEGIN
+	
+	IF pidcategory > 0 THEN
+		
+		UPDATE tb_categories
+        SET descategory = pdescategory
+        WHERE idcategory = pidcategory;
+        
+    ELSE
+		
+		INSERT INTO tb_categories (descategory) VALUES(pdescategory);
+        
+        SET pidcategory = LAST_INSERT_ID();
+        
+    END IF;
+    
+    SELECT * FROM tb_categories WHERE idcategory = pidcategory;
+    
+END$$
+
+DELIMITER ;
